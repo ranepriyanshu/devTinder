0db0d253f8967891bba1250f38bc41fcd19ed81f
ranepriyanshu <priyanshurane09@gmail.com>
2025-08-30 00:50:35 +0530

    nodemon

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
-
 const app = express();
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.get('/', (req, res) => {
+  res.send('Hello, Express is working!');
+});
+
+app.listen(7777, () => {
+  console.log('Server running at http://localhost:7777');
+});
