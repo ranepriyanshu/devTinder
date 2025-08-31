663da88d1276e67cd90e53f0457911d7aa6b207a
ranepriyanshu <priyanshurane09@gmail.com>
2025-08-31 15:58:47 +0530

    creating APIs😉

--- src/app.js
+++ src/app.js
@@ -3,6 +3,108 @@ const app = express();
 
 const connectDB = require('./config/database');
 
+const User = require('./models/user');
+
+
+// creating api using post  (sign up)
+
+app.use(express.json()); // a middle ware to parse the json data in simple words it coverts json data to object
+
+
+app.post("/signup", async (req, res)=>{
+
+// handling the data dynamically 
+
+ const user = new User(req.body)
+
+ try{
+    await user.save();
+    res.send("user added successfully");
+ }catch(err){
+    res.status(400).send("Error saving user"+ err);w
+    
+ }
+  
+  // const userObj = {
+
+  //     firstName: "Priyanshu",
+  //     lastName: "Rane",
+  //     password: "123456",
+  //     email: "pV5Tt@example.com",
+  //     gender: "male"
+  // } 
+  // // creating new instance of user Model 
+  // const user = new User(userObj);
+
+  // try{
+  //   await user.save();
+  //   res.send("user added successfully");
+  // }catch(err){
+  //   res.status(400).send("Error saving user"+ err);
+  // }
+
+
+})
+
+
+// getting user  by emailid
+
+app.get("/user", async (req, res)=>{
+
+    const userEmail = req.body.email;
+
+    try{
+      const user = await User.find({email: userEmail});
+      res.send(user);
+    }catch(err){
+      res.status(400).send("Error saving user"+ err);
+    }
+})
+
+
+// feed API , finding all the data 
+
+app.get("/feed", async (req, res)=>{
+
+    try{
+      const users = await User.find({});
+      res.send(users);
+    }catch(err){
+      res.status(400).send("Error saving user"+ err); 
+    }
+})
+
+// deleting the user
+
+app.delete("/user", async (req, res)=>{
+
+  const userId = req.body.userId;
+  try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  }catch(err){
+    res.status(400).send("Error deleting user"+ err);
+  }
+});
+
+app.patch("/user", async (req, res)=>{
+
+  const userId = req.body.userId; // here is requesting userID
+  const data = req.body;  // updated data;
+
+  try{
+     await User.findByIdAndUpdate({_id: userId}, data);
+     res.send("user updated successfully");
+  }catch(err){
+    res.status(400).send("Error updating user"+ err);
+  }
+ 
+  
+})
+
+
+// updating the user details
+
 
 connectDB()
    .then(()=>{
